9d2800fcbaa2f19166fef95e88e18e5e8cc93c78
R3ym0nd0 <raymondjoaquin50@gmail.com>
2025-07-20 01:23:29 +0800

    v0.0.1: First Release

---
+++ assets/js/hamburger_bar.js
@@ -0,0 +1,12 @@
+const hamburgerBar = document.querySelector("#hamburger-icon");
+const navBar = document.querySelector("nav ul");
+
+hamburgerBar.addEventListener("click", () => {
+    if (navBar.classList.contains("show")) {
+        hamburgerBar.style.color = "white";
+        navBar.classList.remove("show")
+    } else {
+        hamburgerBar.style.color = "rgb(84, 207, 255)"
+        navBar.classList.add("show")
+    }
+})  
